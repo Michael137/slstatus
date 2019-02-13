2585e3455e4f10c9d49bf23a67159371895c80b2
Michael Buch <michaelbuch12@gmail.com>
2019-02-13 00:58:56 +0000

    update with wifi perc component

--- components/entropy.c
+++ components/entropy.c
@@ -1,10 +1,10 @@
 /* See LICENSE file for copyright and license details. */
+#include <stdint.h>
+#include <stdio.h>
+
+#include "../util.h"
+
 #if defined(__linux__)
-	#include <stdint.h>
-	#include <stdio.h>
-
-	#include "../util.h"
-
 	const char *
 	entropy(void)
 	{
@@ -17,7 +17,33 @@
 
 		return bprintf("%ju", num);
 	}
-#elif defined(__OpenBSD__)
+//#elif defined(__FreeBSD__)
+//	#include <stdlib.h>
+//	#include <sys/ioctl.h>
+//	#include <sys/rndio.h>
+//
+//	const char *
+//	entropy(void)
+//	{
+//		int entropy;
+//		int fd;
+//
+//		fd = open("/dev/random", O_RDONLY);
+//		if (fd < 0) {
+//			warn("open '/dev/random':");
+//			return 0;
+//		}
+//
+//		if (ioctl(fd, RNDGETENTCNT, &entropy) < 0) {
+//			warn("ioctl 'RNDGETENTCNT':");
+//			close(fd)
+//			return NULL;
+//		}
+//
+//		close(fd)
+//		return bprintf("%ju", entropy);
+//	}
+#elif defined(__OpenBSD__) | defined(__FreeBSD__)
 	const char *
 	entropy(void)
 	{
